cd2c5b1ec433712edd9a807aec324a33adbc2573
sinno <sinno.song@outlook.com>
2025-02-16 15:17:06 +0800

    fix: fix format

--- YoutubeDownloader/ViewModels/Dialogs/DownloadMultipleSetupViewModel.cs
+++ YoutubeDownloader/ViewModels/Dialogs/DownloadMultipleSetupViewModel.cs
@@ -41,8 +41,7 @@ public partial class DownloadMultipleSetupViewModel(
     public IReadOnlyList<Container> AvailableContainers { get; } =
         [Container.Mp4, Container.WebM, Container.Mp3, new Container("ogg")];
 
-    public IReadOnlyList<VideoQualityPreference> AvailableVideoQualityPreferences { get; } =
-        Enum.GetValues<VideoQualityPreference>().Reverse().ToArray();
+    public IReadOnlyList<VideoQualityPreference> AvailableVideoQualityPreferences { get; } = Enum.GetValues<VideoQualityPreference>().Reverse().ToArray();
 
     [RelayCommand]
     private void Initialize()
